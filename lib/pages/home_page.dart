08def946553b34514d67e8275e9e02f361e7dee0
ManifestJosh <okeugochijoshua@gmail.com>
2024-03-07 02:06:58 +0100

    new updated commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/main.dart';
+import 'package:todo_app/widgets/add_todo_dialogue.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final tabs = [
+      Container(),
+      Container(),
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(MyApp.title),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white.withOpacity(0.6),
+        selectedItemColor: Colors.white,
+        currentIndex: selectedIndex,
+        onTap: (index) => setState(() {
+          selectedIndex = index;
+        }),
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fact_check_outlined),
+            label: 'Todos',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.done, size: 28),
+            label: 'Completed',
+          ),
+        ],
+      ),
+      body: tabs[selectedIndex],
+      floatingActionButton: FloatingActionButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        backgroundColor: Colors.purple[800],
+        onPressed: () => showDialog(
+          context: context,
+          builder: (context) => AddTodoDialogWidget(),
+          barrierDismissible: false,
+        ),
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
