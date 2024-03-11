c96503f909c36db7dbb39b9fd21ff72fcdf77e1b
ManifestJosh <okeugochijoshua@gmail.com>
2024-03-11 04:55:35 +0100

    added savetodo function

--- lib/pages/edit_todo_page.dart
+++ lib/pages/edit_todo_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:todo_app/model/todo.dart';
 import 'package:todo_app/provider/todos.dart';
 import 'package:todo_app/widgets/todo_form_widget.dart';
@@ -12,6 +13,7 @@ class EditTodopage extends StatefulWidget {
 }
 
 class _EditTodopageState extends State<EditTodopage> {
+  final _formKey = GlobalKey<FormState>();
   late String title;
   late String description;
 
@@ -27,12 +29,44 @@ class _EditTodopageState extends State<EditTodopage> {
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
           title: Text('Edit Todo'),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () {
+                final provider =
+                    Provider.of<TodosProvider>(context, listen: false);
+                provider.removeTodo(widget.todo);
+
+                Navigator.of(context).pop();
+              },
+            )
+          ],
         ),
-        body: TodoFormWidget(
-          onChangedTitle: (title) => setState(() => this.title = title),
-          onChangedDescription: (description) =>
-              setState(() => this.description = description),
-          onSavedTodo: () {},
+        body: Padding(
+          padding: EdgeInsets.all(16),
+          child: Form(
+            key: _formKey,
+            child: TodoFormWidget(
+              title: title,
+              onChangedTitle: (title) => setState(() => this.title = title),
+              onChangedDescription: (description) =>
+                  setState(() => this.description = description),
+              onSavedTodo: saveTodo,
+            ),
+          ),
         ),
       );
+  void saveTodo() {
+    final isValid = _formKey.currentState?.validate();
+
+    if (!isValid!) {
+      return;
+    } else {
+      final provider = Provider.of<TodosProvider>(context, listen: false);
+
+      provider.updateTodo(widget.todo, title, description);
+
+      Navigator.of(context).pop();
+    }
+  }
 }
